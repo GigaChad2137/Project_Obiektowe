ffe00401dcefebba07688f2be75de020d4738c3a
Klaudiusz Kolegowicz <klaudiusz.kolegowicz@microsoft.wsei.edu.pl>
2022-05-27 13:08:08 +0200

    Główne okmo Main Window z bindowane z widokiem home,usunięty błąd z niepoprawną ściężką

---
+++ Project/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Project.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Project.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+  
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+           ;
+            HomeVM = new HomeViewModel();
+          
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+           
+        }
+    }
+}
