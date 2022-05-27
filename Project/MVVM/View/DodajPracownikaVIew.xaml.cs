7cca13eb427b0eaa9f7db2c9020bcbf639988e7f
Klaudiusz Kolegowicz <klaudiusz.kolegowicz@microsoft.wsei.edu.pl>
2022-05-27 14:03:05 +0200

    Formularz  Dodający pracownika,Wyświetlanie Pracowników i automatyczne dodawanie ich do tabeli (praca) jeśli nie istnieli w bieżącym dniu,Widok zwykłego Pracownika

---
+++ Project/MVVM/View/DodajPracownikaVIew.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project.MVVM.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DodajPracownikaVIew.xaml
+    /// </summary>
+    public partial class DodajPracownikaVIew : UserControl
+    {
+        public DodajPracownikaVIew()
+        {
+            InitializeComponent();
+        }
+        private void Register_btnclick_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] Source;
+            byte[] hashed_Data;
+            var reg_user = Register_username.Text;
+            var reg_passwd = Register_password.Password;
+
+            //To do : zmienic SqlConnection na framework Entity
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+            SqlTransaction transaction;
+
+            if (sqlCon.State == System.Data.ConnectionState.Closed)
+                sqlCon.Open();
+
+            try
+            {
+                transaction = sqlCon.BeginTransaction();
+                string query = "SELECT COUNT(1) from users Where username=@username";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon, transaction);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@username", Register_username.Text);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MessageBox.Show("Podany użytkownik już istnieje");
+                }
+                else
+                {
+                    if (reg_user != "" && reg_user.Length > 4)
+                    {
+                        if (reg_passwd == Register_retypePassword.Password && reg_passwd != "" && reg_passwd.Length > 4 && reg_passwd.Length < 20)
+                        {
+                            Source = ASCIIEncoding.ASCII.GetBytes(Register_retypePassword.Password);
+                            hashed_Data = new MD5CryptoServiceProvider().ComputeHash(Source);
+                            string passwd_hash = Convert.ToBase64String(hashed_Data);
+
+
+
+                            string query_user = "Insert Into users (username,password) values(@username,@password)";
+                            SqlCommand sql_register = new SqlCommand(query_user, sqlCon, transaction);
+                            sql_register.CommandType = System.Data.CommandType.Text;
+                            sql_register.Parameters.AddWithValue("@username", Register_username.Text);
+                            sql_register.Parameters.AddWithValue("@password", passwd_hash);
+                            sql_register.ExecuteNonQuery();
+                            transaction.Commit();
+
+                            string query_for_id = "Select Id from users where username=@username";
+                            SqlCommand sql_id = new SqlCommand(query_for_id, sqlCon, transaction);
+                            sql_id.CommandType = System.Data.CommandType.Text;
+                            sql_id.Parameters.AddWithValue("@username", Register_username.Text);
+                            int To = Convert.ToInt32(sql_id.ExecuteScalar());
+
+
+                            string query_rola = "Insert Into user_roles (id_user,id_role) values(@id,@rola)";
+                            SqlCommand sql_rola = new SqlCommand(query_rola, sqlCon);
+                            sql_rola.Parameters.AddWithValue("@id", To);
+                            if (Register_czy_szef.IsChecked == true)
+                            {
+                                int rola = 2;
+                                sql_rola.Parameters.AddWithValue("@rola", rola);
+                            }
+                            else
+                            {
+                                int rola = 1;
+                                sql_rola.Parameters.AddWithValue("@rola", rola);
+                            }
+                            sql_rola.ExecuteNonQuery();
+                            MessageBox.Show("Pracownik dodany");
+
+
+                        }
+                        else if (reg_passwd == "")
+                        {
+                            MessageBox.Show("Hasło nie może być puste");
+                        }
+                        else if (reg_passwd.Length < 4 || reg_passwd.Length > 20)
+                        {
+                            MessageBox.Show("Hasło musi zawierać się miedzy 4 a 20 znaków.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Hasła nie są takie same");
+                        }
+
+                    }
+                    else if (reg_user.Length < 4 || reg_user.Length > 20)
+                    {
+                        MessageBox.Show("Użytkownik musi zawierać się miedzy 4 a 20 znaków.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Hasło nie może być puste");
+                    }
+
+                }
+
+
+            }
+            catch (Exception ea)
+            {
+                MessageBox.Show(ea.Message);
+
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+        }
+    }
+}
---
+++ Project/MVVM/View/PracownicyView.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Project.MVVM.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PracownicyView.xaml
+    /// </summary>
+    public partial class PracownicyView : UserControl
+    {
+        public PracownicyView()
+        {
+            InitializeComponent();
+            // Todo: podlaczyc baze danych i zaladowac modele
+            //DBPROJECT db = new DBPROJECT();
+
+
+            //var id_finder = (from users in db.users
+            //                 join user_roles in db.user_roles on users.Id equals user_roles.id_user
+            //                 join roles in db.roles on user_roles.id_role equals roles.Id
+            //                 join zarobki in db.zarobki on users.Id equals zarobki.Id_pracownika
+            //                 select new
+            //                 {
+            //                     Id = users.Id,
+            //                     Name = users.username,
+            //                     Rola = roles.role,
+            //                     Zarobas = zarobki.Zarobki1,
+
+
+            //                 }).ToList();
+            //foreach (var p in id_finder)
+            //{
+
+            //    DateTime thisDay = DateTime.Today;
+            //    if (db.praca.Where(c => c.Id_pracownika == p.Id && c.Data == thisDay).Count() > 0)
+            //    {
+            //        var sprawdzacz_czy_pracuje = from c in db.praca where c.Id_pracownika == p.Id && c.Data == thisDay select c;
+            //        var praca_checker = sprawdzacz_czy_pracuje.FirstOrDefault<praca>();
+
+
+            //        PracownicyGrid.Items.Add(new Pracownicy
+            //        {
+            //            id_pracownika = p.Id,
+            //            Nazwa_pracownika = p.Name,
+            //            Rola_pracownika = p.Rola,
+            //            Czy_pracuje = (bool)praca_checker.Czy_pracuje,
+            //            Zarobki_Pracownika = p.Zarobas
+            //        }
+            //        );
+            //    }
+            //    else
+            //    {
+
+            //        var pracownik = db.Set<praca>();
+            //        db.praca.Add(new praca { Id_pracownika = p.Id, Data = default, Data_rozpoczecia = null, Data_zakonczenia = null, Czy_pracuje = false });
+            //        db.SaveChanges();
+
+            //        PracownicyGrid.Items.Add(new Pracownicy
+            //        {
+            //            id_pracownika = p.Id,
+            //            Nazwa_pracownika = p.Name,
+            //            Rola_pracownika = p.Rola,
+            //            Czy_pracuje = false,
+            //            Zarobki_Pracownika = p.Zarobas
+            //        });
+            //    }
+
+
+
+            //}
+
+
+        }
+
+
+        public class Pracownicy
+        {
+
+            public int id_pracownika { get; set; }
+            public string Nazwa_pracownika { get; set; }
+            public string Rola_pracownika { get; set; }
+            public bool Czy_pracuje { get; set; }
+            public int Zarobki_Pracownika { get; set; }
+        }
+
+        private void PracownicyGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
