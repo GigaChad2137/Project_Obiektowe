79cd9212e9c10f8e0c2116e3cc3c663f07bc004d
Klaudiusz Kolegowicz <klaudiusz.kolegowicz@microsoft.wsei.edu.pl>
2022-06-27 14:11:18 +0200

    Ukończone Oknow WOrkRaportPdf - dane wyciągane z  bazy danych

--- Project/MVVM/View/WorkRaportPdf.xaml.cs
+++ Project/MVVM/View/WorkRaportPdf.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,14 +23,51 @@ namespace Project.MVVM.View
         public WorkRaportPdf()
         {
             InitializeComponent();
-            List<Pdf_view> items = new List<Pdf_view>();
-            items.Add(new Pdf_view() { Name = "John Doe", czas_start = DateTime.Now,czas_stop=DateTime.Now,godziny="4h",kwota="80zloty" });
-            items.Add(new Pdf_view() { Name = "John Doe", czas_start = DateTime.Now, czas_stop = DateTime.Now, godziny = "4h", kwota = "80zloty" });
-            items.Add(new Pdf_view() { Name = "John Doe", czas_start = DateTime.Now, czas_stop = DateTime.Now, godziny = "4h", kwota = "80zloty" });
+            fillList();
 
-            Listviewpdf.ItemsSource = items;
+
+
+
+
         }
 
+        private void fillList()
+        {
+            List<Pdf_view> items = new List<Pdf_view>();
+            int id_currect_user = (int)Application.Current.Properties["currect_user_id"];
+            var today = DateTime.Today;
+            var month = new DateTime(today.Year, today.Month, 1);
+            var first = month.AddMonths(-0);
+            var last = month.AddDays(+29);
+            double suma_miesiac = 0;
+            using (var db = new DBPROJECT())
+            {
+                using (var contex = db.Database.BeginTransaction())
+                {
+                    
+                    var dane_usera = db.informacje_personalne.Where(x => x.Id_pracownika == id_currect_user).First();
+                    double zarobek_na_godzine = dane_usera.Zarobki / 160;
+                    var miesiac_rozliczenia = db.praca.Where(x => x.Id_pracownika == id_currect_user && x.Data >= first && x.Data <= last).ToList();
+                    foreach (var dzien in miesiac_rozliczenia)
+                    {
+                       // dzien.Data_rozpoczecia.Value.TimeOfDay
+                        TimeSpan? godziny_przepracowane = dzien.Data_zakonczenia - dzien.Data_rozpoczecia;
+                      
+                        Trace.WriteLine(godziny_przepracowane);
+                        double suma_dzien =Math.Round(godziny_przepracowane.Value.TotalHours * zarobek_na_godzine,2);
+                        items.Add(new Pdf_view { data = dzien.Data.ToString(), czas_start = dzien.Data_rozpoczecia.Value.TimeOfDay, czas_stop = dzien.Data_zakonczenia.Value.TimeOfDay,godziny= $"{Math.Round(godziny_przepracowane.Value.TotalHours,2)}h",kwota=$"{suma_dzien}zł" });
+                        suma_miesiac = suma_miesiac + suma_dzien;
+                    }
+                    Listviewpdf.ItemsSource = items;
+                    Data_stworzenia_pdfa.Text = $"{DateTime.Today.ToShortDateString()}";
+                    Podsumowanie_wyplaty.Text = $"{suma_miesiac}zł";
+                    imie_nazwisko_pracownika.Text = $"{dane_usera.Imie} {dane_usera.Nazwisko}";
+                    Data_rozliczenia.Text = $"{first.ToShortDateString()}-{last.ToShortDateString()}";
+
+                }
+            }
+         
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -50,9 +88,9 @@ namespace Project.MVVM.View
     }
     public class Pdf_view
     {
-        public string Name { get; set; }
-        public DateTime czas_start { get; set; }
-        public DateTime czas_stop { get; set; }
+        public string data { get; set; }
+        public TimeSpan? czas_start { get; set; }
+        public TimeSpan? czas_stop { get; set; }
         public string godziny { get; set; }
         public string kwota { get; set; }
 
